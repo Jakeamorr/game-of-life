15a7c2d18e6381341600ccbb395f78dcfe6b9f82
Jake Morris <Jakeamorr@gmail.com>
2022-01-13 16:07:41 -0500

    add logic to randomly seed the board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,24 @@
 import Cell from "./Cell";
 
+// When starting a new game light all of the cells in a snake light pattern starting from the edges inward
 function Board() {
-  const ARRAY_SIZE = 100;
-  const grid = new Array(ARRAY_SIZE);
-  grid.fill(new Array(ARRAY_SIZE));
+  const ARRAY_SIZE = 10;
+  const temp = new Array(ARRAY_SIZE).fill(null);
+  const grid = temp.map((row) => randomArray(ARRAY_SIZE));
 
-  // for (let i = 0; i < ARRAY_SIZE; i++) {
-  //   for (let j = 0; j < ARRAY_SIZE; j++) {
-  //     grid[i][j] = Math.floor(Math.random() * 100);
-  //   }
-  // }
-  grid.forEach((row) => row.forEach((cell) => Math.floor(Math.random() * 100)));
-  const cells = grid.map((row, i) => <Cell key={i} on={row[0] > 90} />);
-  console.log(grid);
-  // When starting a new game light all of the cells in a snake light pattern starting from the edges inward
+  function randomNum() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  function randomArray() {
+    const arr = [];
+    for (let i = 0; i < ARRAY_SIZE; i++) {
+      arr.push(randomNum() > 5 ? true : false);
+    }
+    return arr;
+  }
+
+  const cells = grid.map((row, i) => <Cell key={i} on={row[0]} />);
   return <div className="board">{cells}</div>;
 }
 
