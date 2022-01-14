d97b4c7f20a1e70300e1aeb0971d4aa2b0693a52
Jake Morris <Jakeamorr@gmail.com>
2022-01-14 10:13:23 -0500

    trying to add logic for game loop

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import Row from "./Row";
 
 // When starting a new game light all of the cells in a snake light pattern starting from the edges inward
 function Board() {
   const ARRAY_SIZE = 90;
   const temp = new Array(ARRAY_SIZE).fill(null);
-  const grid = temp.map((row) => randomArray(ARRAY_SIZE));
+  const [grid, setGrid] = useState(temp.map((row) => randomArray(ARRAY_SIZE)));
+
+  useEffect(() => {
+    function checkNeighbors() {
+      // Any live cell with two or three live neighbours survives.
+      // Any dead cell with three live neighbours becomes a live cell.
+      // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+      return grid.map((row, i) => {
+        return row.map((cell, j) => {
+          let live = 0;
+          // if (j - 0 >= 0) live++;
+          live = Math.floor(Math.random() * 8);
+
+          if (cell && (live === 2 || live === 3)) return true;
+          else if (!cell && live === 3) return true;
+          else return false;
+        });
+      });
+    }
+    const loopId = setTimeout(() => {
+      const newGrid = checkNeighbors();
+      console.log(newGrid === grid);
+      setGrid(newGrid);
+    }, 1000);
+    return clearInterval(loopId);
+  }, []);
 
   function randomNum() {
     return Math.floor(Math.random() * 10);
@@ -18,22 +44,8 @@ function Board() {
     return arr;
   }
 
-  function checkNeighbors() {
-    // Any live cell with two or three live neighbours survives.
-    // Any dead cell with three live neighbours becomes a live cell.
-    // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid.length; j++) {
-        let live = 0;
-        if (j - 0 >= 0) live++;
-
-        if (grid[i][j] && (live === 2 || live === 3)) return true;
-        else if (!grid[i][j] && live === 3) return true;
-        else return false;
-      }
-    }
-  }
   const cells = grid.map((row, i) => <Row key={i} cells={row} />);
+  console.log("grid rendered");
   return <div className="board">{cells}</div>;
 }
 
