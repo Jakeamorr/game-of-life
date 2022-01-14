e61153bbc9bec276f424aca1dea04e64b3fcbcd8
Jake Morris <Jakeamorr@gmail.com>
2022-01-14 12:48:15 -0500

    add link to the wiki page for conway's game

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Board from "./components/Board";
 function App() {
   return (
     <div className="app">
+      <a
+        href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life"
+        target="_blank"
+        rel="noreferrer"
+        className="game-title"
+      >
+        Conway's Game of Life
+      </a>
       <Board />
     </div>
   );
