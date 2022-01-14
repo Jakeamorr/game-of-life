ce770737167d6757be0221ee754511bfd5e06009
Jake Morris <Jakeamorr@gmail.com>
2022-01-14 12:13:03 -0500

    finish game logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,41 @@
 import { useEffect, useState } from "react";
 import Row from "./Row";
 
-// When starting a new game light all of the cells in a snake light pattern starting from the edges inward
 function Board() {
-  const ARRAY_SIZE = 90;
-  const temp = new Array(ARRAY_SIZE).fill(null);
-  const [grid, setGrid] = useState(temp.map((row) => randomArray(ARRAY_SIZE)));
+  const ARRAY_HEIGHT = 70;
+  const ARRAY_WIDTH = 120;
+  const temp = new Array(ARRAY_WIDTH).fill(null);
+  const [grid, setGrid] = useState(
+    temp.map((row) => randomArray(ARRAY_HEIGHT))
+  );
 
   useEffect(() => {
-    function checkNeighbors() {
-      // Any live cell with two or three live neighbours survives.
-      // Any dead cell with three live neighbours becomes a live cell.
-      // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-      return grid.map((row, i) => {
-        return row.map((cell, j) => {
+    // Check whether a cell lives or dies
+    function checkCells() {
+      return grid.map((column, i) => {
+        return column.map((cell, j) => {
+          // Check how many of the cell's neighbors are alive
           let live = 0;
-          // if (j - 0 >= 0) live++;
-          live = Math.floor(Math.random() * 8);
+          // check top
+          if (grid[i][j - 1]) live++;
+          // check bottom
+          if (grid[i][j + 1]) live++;
+          // check left
+          if (grid[i - 1] && grid[i - 1][j]) live++;
+          // check right
+          if (grid[i + 1] && grid[i + 1][j]) live++;
+          // check top left diagonal
+          if (grid[i - 1] && grid[i - 1][j - 1]) live++;
+          // check bottom left diagonal
+          if (grid[i + 1] && grid[i + 1][j - 1]) live++;
+          // check top right diagonal
+          if (grid[i - 1] && grid[i - 1][j + 1]) live++;
+          // check bottom right diagonal
+          if (grid[i + 1] && grid[i + 1][j + 1]) live++;
 
+          // Any live cell with two or three live neighbours survives.
+          // Any dead cell with three live neighbours becomes a live cell.
+          // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
           if (cell && (live === 2 || live === 3)) return true;
           else if (!cell && live === 3) return true;
           else return false;
@@ -25,10 +43,9 @@ function Board() {
       });
     }
     const interval = setInterval(() => {
-      const newGrid = checkNeighbors();
-      console.log(newGrid === grid);
+      const newGrid = checkCells();
       setGrid(newGrid);
-    }, 1000);
+    }, 200);
     return () => clearInterval(interval);
   });
 
@@ -38,15 +55,19 @@ function Board() {
 
   function randomArray() {
     const arr = [];
-    for (let i = 0; i < ARRAY_SIZE; i++) {
+    for (let i = 0; i < ARRAY_HEIGHT; i++) {
       arr.push(randomNum() > 5 ? true : false);
     }
     return arr;
   }
 
-  const cells = grid.map((row, i) => <Row key={i} cells={row} />);
-  console.log("grid rendered");
-  return <div className="board">{cells}</div>;
+  return (
+    <div className="board">
+      {grid.map((row, i) => (
+        <Row key={i} cells={row} />
+      ))}
+    </div>
+  );
 }
 
 export default Board;
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,6 +1,5 @@
 function Cell(props) {
   const cellClasses = "cell".concat(props.on ? " alive" : "");
-  // const cellClasses = "cell alive";
   return <div className={cellClasses}></div>;
 }
 
